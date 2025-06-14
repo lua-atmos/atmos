3f1e050bd6cadbb558da5ccfe7d2bd56948fdda5
Francisco Sant'Anna <francisco.santanna@gmail.com>
2025-06-14 01:24:04 -0300

    - loop ren

--- atmos/env/sdl.lua
+++ atmos/env/sdl.lua
@@ -1,6 +1,6 @@
 local SDL = require "SDL"
 
-local env = {}
+local M = {}
 
 local meta = {
     __atmos = function (e, awt)
@@ -12,7 +12,11 @@ local MS_PER_FRAME = 40
 local old = SDL.getTicks() - MS_PER_FRAME
 local ms = 0
 
-function env.loop ()
+function M.point_vs_rect (p, r)
+    return SDL.hasIntersection(r, { x=p.x, y=p.y, w=1, h=1 })
+end
+
+function M.loop (ren)
     while true do
         local e = SDL.waitEvent(ms)
         if e then
@@ -28,8 +32,14 @@ function env.loop ()
                 emit('step', MS_PER_FRAME)
             end
         end
+        if ren then
+            ren:clear()
+        end
         emit('SDL.Draw')
+        if ren then
+            ren:present()
+        end
     end
 end
 
-return env
+return M
